a3971d5c8beda3ac5ea9ef6192d13ff58b6ade3b
sehoon.gim <sehoongim@gmail.com>
2025-10-25 20:19:57 +0900

    로직 수정
     - 이미 적용한 커밋은 적용하지 않는다
     - 최근 일주일간 변경 내역만 patch 파일로 만든다

--- sync.rb
+++ sync.rb
@@ -10,8 +10,10 @@ end
 dir_lst.each do |dir|
   Dir.chdir(File.join('..', dir)) do |work_dir|
     puts "we're now in: #{work_dir}"
+    cmts = `git rev-list --since="1 weekk ago" HEAD`.strip.split("\n")
+    oldest, newest = cmts.last, cmts.first
     puts "save patch files to: #{File.join('..' + '/..' * (dir.split('/').size - 1), 'patch_root', dir)}"
-    `git format-patch --root -o #{File.join('..' + '/..' * (dir.split('/').size - 1), 'patch_root', dir)}`
+    `git format-patch #{oldest}^..#{newest} -o #{File.join('..' + '/..' * (dir.split('/').size - 1), 'patch_root', dir)}`
   end
 end
 
